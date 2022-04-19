e71c76adaea04005230af157c30f074c76afce4f
CarlosFonseca97 <carlos_loscar.9@hotmail.com>
2022-04-19 09:16:57 -0500

    code2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.referencecode.database.R
-abstract class MainActivity : AppCompatActivity() {
 
-    companion object {
-        private const val TAG = "KotlinActivity"
+class MainActivity : AppCompatActivity() {
 
-    val database = Firebase.database
-    val myRef = database.getReference("message")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var database = Firebase.database
+        var myRef = database.getReference("message")
 
-    myRef.setValue("Hello, World!")
+        myRef.setValue("Hello, World!")
+    }
+
+
     }
